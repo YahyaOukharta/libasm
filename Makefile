f8c9ebdfab0a9c7a42e559ef10cd3f063c811db4
Yahya Oukharta <youkhart@e1r5p9.1337.ma>
2020-01-31 20:37:59 +0100

    ...

--- Makefile
+++ Makefile
@@ -5,7 +5,23 @@ FLAGS = -f macho64
 SRCS = ft_strlen.s ft_strcpy.s ft_strcmp.s \
 	   ft_write.s ft_read.s ft_strdup.s
 
-all: $(SRCS)
+OBJS = $(SRCS:.s=.o)
 
-clean :
-	rm -rf $(SRCS:.s=.o)
+all: $(NAME)
+
+$(NAME):
+	$(foreach file, $(SRCS), nasm $(FLAGS) $(file);)
+	ar -rc $(NAME) *.o 
+
+clean:
+	rm -f a.out
+	rm -rf *.o
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+c : re
+	gcc main.c -L. -lasm
+	./a.out
---
+++ libasm.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libasm.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: youkhart <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/31 20:04:36 by youkhart          #+#    #+#             */
+/*   Updated: 2020/01/31 20:28:31 by youkhart         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strcpy(char *dst,const char *src);
+size_t	ft_strlen(const char *str);
+int		ft_strcmp(const char *s1, const char *s2);
+char	*ft_strdup(const char *str);
+ssize_t	ft_write(int fd, const void *buf, size_t n_bytes);
+ssize_t	ft_read(int fd, const void *buf, size_t n_bytes);
+
